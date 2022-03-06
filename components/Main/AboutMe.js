b8f041b145c7ae3f7c0bb6a85afb6691743a833d
eeguney <emreguney@outlook.com.tr>
2022-03-07 02:06:26 +0300

    Edit

--- components/Main/AboutMe.js
+++ components/Main/AboutMe.js
@@ -4,7 +4,7 @@ import { AUTHOR_NAME, AUTHOR_SHORT_INFO, GITHUB_USERNAME, AUTHOR_EMAIL, TWITTER_
 export const AboutMe = ({ style }) => {
   return (
     <div className={style.aboutMe}>
-        <div className={style.profilePhoto}><img src="https://genki.fueko.net/content/images/size/w800/2021/05/photo-1613420568020-0c0e3764d499.jpeg" alt="Emre Güney" /></div>
+        <div className={style.profilePhoto}><img src="https://avatars.githubusercontent.com/u/73538729?v=4" alt="Emre Güney" /></div>
         <h2>{AUTHOR_NAME}</h2>
         <p>{AUTHOR_SHORT_INFO}</p>
         <div className={style.socialLinks}>
