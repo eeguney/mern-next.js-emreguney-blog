dd62052a1ed3f447277035ccabf585e7abf0c68f
eeguney <emreguney@outlook.com.tr>
2022-03-07 01:04:40 +0300

    Edit

--- constants/constants.js
+++ constants/constants.js
@@ -1,4 +1,4 @@
-export const SITE_ADRESS = "http://localhost:3000"
+export const SITE_ADRESS = "https://mern-next-js-emreguney-blog.vercel.app"
 export const AUTHOR_NAME = "Emre Güney"
 export const AUTHOR_SHORT_INFO = "Trying to be front-end, maybe even full-stack developer..."
 export const GITHUB_USERNAME = "eeguney"
